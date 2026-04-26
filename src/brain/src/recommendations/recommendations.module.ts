b70b1eaa34119b234d7cc0952ffb086ddc205c4c
Cesar Ortiz <cesarjavierortizmontero@gmail.com>
2026-04-26 10:01:05 -0500

    feat(recommendations): wire ciiu-graph-port and dynamic-value-chain-rules in module

--- src/brain/src/recommendations/recommendations.module.ts
+++ src/brain/src/recommendations/recommendations.module.ts
@@ -1,6 +1,7 @@
 import { Module, forwardRef } from '@nestjs/common'
 import { CiiuTaxonomyModule } from '@/ciiu-taxonomy/ciiu-taxonomy.module'
 import { CompaniesModule } from '@/companies/companies.module'
+import { DynamicValueChainRules } from './application/services/DynamicValueChainRules'
 import { AiMatchEngine } from './application/services/AiMatchEngine'
 import { AllianceMatcher } from './application/services/AllianceMatcher'
 import { CandidateSelector } from './application/services/CandidateSelector'
@@ -13,10 +14,12 @@ import { GenerateRecommendations } from './application/use-cases/GenerateRecomme
 import { GetCompanyRecommendations } from './application/use-cases/GetCompanyRecommendations'
 import { GetGroupedCompanyRecommendations } from './application/use-cases/GetGroupedCompanyRecommendations'
 import { AI_MATCH_CACHE_REPOSITORY } from './domain/repositories/AiMatchCacheRepository'
+import { CIIU_GRAPH_PORT } from './domain/ports/CiiuGraphPort'
 import { RECOMMENDATION_REPOSITORY } from './domain/repositories/RecommendationRepository'
 import { CompanyRecommendationsController } from './infrastructure/http/company-recommendations.controller'
 import { RecommendationsController } from './infrastructure/http/recommendations.controller'
 import { SupabaseAiMatchCacheRepository } from './infrastructure/repositories/SupabaseAiMatchCacheRepository'
+import { SupabaseCiiuGraphRepository } from './infrastructure/repositories/SupabaseCiiuGraphRepository'
 import { SupabaseRecommendationRepository } from './infrastructure/repositories/SupabaseRecommendationRepository'
 
 @Module({
@@ -31,6 +34,11 @@ import { SupabaseRecommendationRepository } from './infrastructure/repositories/
       provide: AI_MATCH_CACHE_REPOSITORY,
       useClass: SupabaseAiMatchCacheRepository,
     },
+    {
+      provide: CIIU_GRAPH_PORT,
+      useClass: SupabaseCiiuGraphRepository,
+    },
+    DynamicValueChainRules,
     AiMatchEngine,
     AllianceMatcher,
     CandidateSelector,
@@ -46,6 +54,7 @@ import { SupabaseRecommendationRepository } from './infrastructure/repositories/
   exports: [
     RECOMMENDATION_REPOSITORY,
     AI_MATCH_CACHE_REPOSITORY,
+    CIIU_GRAPH_PORT,
     PeerMatcher,
     ValueChainMatcher,
     AllianceMatcher,

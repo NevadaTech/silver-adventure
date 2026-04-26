4e9f4318034adac4e291f1a96f6e9b13352eafe4
Cesar Ortiz <cesarjavierortizmontero@gmail.com>
2026-04-25 20:10:28 -0500

    feat(companies): add csv company source adapter for hackathon dataset

--- src/brain/src/companies/companies.module.ts
+++ src/brain/src/companies/companies.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common'
 import { FindCompanyById } from './application/use-cases/FindCompanyById'
 import { GetCompanies } from './application/use-cases/GetCompanies'
 import { GetCompaniesUpdatedSince } from './application/use-cases/GetCompaniesUpdatedSince'
+import { SyncCompaniesFromSource } from './application/use-cases/SyncCompaniesFromSource'
 import { COMPANY_REPOSITORY } from './domain/repositories/CompanyRepository'
+import { COMPANY_SOURCE } from './domain/sources/CompanySource'
 import { CompaniesController } from './infrastructure/http/companies.controller'
 import { SupabaseCompanyRepository } from './infrastructure/repositories/SupabaseCompanyRepository'
+import { CsvCompanySource } from './infrastructure/sources/CsvCompanySource'
 
 @Module({
   controllers: [CompaniesController],
@@ -13,15 +16,23 @@ import { SupabaseCompanyRepository } from './infrastructure/repositories/Supabas
       provide: COMPANY_REPOSITORY,
       useClass: SupabaseCompanyRepository,
     },
+    {
+      // Swap to BigQueryCompanySource when the hackathon credentials arrive.
+      provide: COMPANY_SOURCE,
+      useClass: CsvCompanySource,
+    },
     GetCompanies,
     FindCompanyById,
     GetCompaniesUpdatedSince,
+    SyncCompaniesFromSource,
   ],
   exports: [
     COMPANY_REPOSITORY,
+    COMPANY_SOURCE,
     GetCompanies,
     FindCompanyById,
     GetCompaniesUpdatedSince,
+    SyncCompaniesFromSource,
   ],
 })
 export class CompaniesModule {}
